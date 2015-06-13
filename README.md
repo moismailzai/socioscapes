8fd5dbf5bea3cffc1e33d14dfa0b5865470ac767
moismailzai <moismailzai@gmail.com>
2015-06-13 16:08:26 -0400

    fixed spacing

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
- Socioscapes is a javascript alternative to desktop geographic information systems and proprietary data visualization platforms.
- The modular API fuses together various free-to-use and open-source GIS libraries into an organized, modular, and
- sandboxed environment.
+Socioscapes is a javascript alternative to desktop geographic information systems and proprietary data visualization 
+platforms. The modular API fuses various free-to-use and open-source GIS libraries into an organized, modular, and  
+sandboxed environment.
 
-       Source code...................... http://github.com/moismailzai/SocioscapesGIS
-       Reference implementation......... http://app.socioscapes.com
-       License.......................... MIT license (free as in beer & speech)
-       Copyright........................ Ideas are free
- 
-This software was written as partial fulfilment of the degree requirements for the Masters of Arts in Sociology at 
-the University of Toronto.
+   Source code...................... http://github.com/moismailzai/SocioscapesGIS
+   Reference implementation......... http://app.socioscapes.com
+   License.......................... MIT license (free as in beer & speech)
+   Copyright........................ Copies have no rights
+
+This software was written as partial fulfilment of the degree requirements for the Masters of Arts in Sociology at the 
+University of Toronto.
--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 /**
- * Socioscapes is a javascript alternative to desktop geographic information systems and proprietary data visualization platforms.
- * The modular API fuses together various free-to-use and open-source GIS libraries into an organized, modular, and
+ * Socioscapes is a javascript alternative to desktop geographic information systems and proprietary data visualization
+ * platforms. The modular API fuses various free-to-use and open-source GIS libraries into an organized, modular, and
  * sandboxed environment.
  *
- *      Source code...................... http://github.com/moismailzai/SocioscapesGIS
- *      Reference implementation......... http://app.socioscapes.com
- *      License.......................... MIT license (free as in beer & speech)
- *      Copyright........................ Ideas are free
+ *    Source code...................... http://github.com/moismailzai/SocioscapesGIS
+ *    Reference implementation......... http://app.socioscapes.com
+ *    License.......................... MIT license (free as in beer & speech)
+ *    Copyright........................ Copies have no rights
  *
- * This software was written as partial fulfilment of the degree requirements for the Masters of Arts in Sociology at
- * the University of Toronto.
+ * This software was written as partial fulfilment of the degree requirements for the Masters of Arts in Sociology at the
+ * University of Toronto.
  */
 var fetchGoogleAuth = require('./fetchers/fetchGoogleAuth.js'),
     fetchGoogleGeocode = require('./fetchers/fetchGoogleGeocode.js'),
