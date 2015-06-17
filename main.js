ad3902422e736ac61742b0fe6787e4e1ca26a62b
moismailzai <moismailzai@gmail.com>
2015-06-16 20:25:19 -0400

    various jscode updates use strict mode

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
+'use strict';
 /**
  * Socioscapes is a javascript alternative to desktop geographic information systems and proprietary data visualization
  * platforms. The modular API fuses various free-to-use and open-source GIS libraries into an organized, modular, and
- * sandboxed environment.
+ * extendable sandbox.
  *
  *    Source code...................... http://github.com/moismailzai/socioscapes
  *    Reference implementation......... http://app.socioscapes.com
  *    License.......................... MIT license (free as in beer & speech)
- *    Copyright........................ Copies have no rights
+ *    Copyright........................
  *
  * This software was written as partial fulfilment of the degree requirements for the Masters of Arts in Sociology at the
  * University of Toronto.
  */
-
 var fetchGoogleAuth = require('./fetchers/fetchGoogleAuth.js'),
     fetchGoogleGeocode = require('./fetchers/fetchGoogleGeocode.js'),
     fetchGoogleBq = require('./fetchers/fetchGoogleBq.js'),
@@ -19,30 +19,37 @@ var fetchGoogleAuth = require('./fetchers/fetchGoogleAuth.js'),
     newLayer = require('./core/newLayer.js'),
     viewGmap = require('./views/viewGmap.js');
 
-module.exports = function (pluginModule) {
-    var s = {};
-
-    Object.defineProperty(s, 'fetchGoogleAuth', {
+/**
+ * This is the root Socioscapes namespace and object.
+ *
+ * Requires the modules {@link module:fetchGoogleAuth}, {@link module:fetchGoogleGeocode},
+ * {@link module:fetchGoogleBq}, {@link module:fetchWfs}, {@link module:newLayer}, and {@link module:viewGmap}.
+ *
+ * @namespace Socioscapes
+ * @requires module:fetchGoogleAuth
+ * @requires module:fetchGoogleGeocode
+ * @requires module:fetchGoogleBq
+ * @requires module:fetchWfs
+ * @requires module:newLayer
+ * @requires module:viewGmap
+ */
+module.exports = function () {
+    Object.defineProperty(this, 'fetchGoogleAuth', {
         value: fetchGoogleAuth
     });
-    Object.defineProperty(s, 'fetchGoogleGeocode', {
+    Object.defineProperty(this, 'fetchGoogleGeocode', {
         value: fetchGoogleGeocode
     });
-    Object.defineProperty(s, 'fetchGoogleBq', {
+    Object.defineProperty(this, 'fetchGoogleBq', {
         value: fetchGoogleBq
     });
-    Object.defineProperty(s, 'fetchWfs', {
+    Object.defineProperty(this, 'fetchWfs', {
         value: fetchWfs
     });
-    Object.defineProperty(s, 'newLayer', {
+    Object.defineProperty(this, 'newLayer', {
         value: newLayer
     });
-    Object.defineProperty(s, 'viewGmap', {
+    Object.defineProperty(this, 'viewGmap', {
         value: viewGmap
     });
-    if (pluginModule) {
-        return pluginModule(s);
-    }
-
-    return s;
 };
