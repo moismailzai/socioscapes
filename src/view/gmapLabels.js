9f34cebf435136a33e3eda1ea493b6be440c8ae7
moismailzai <moismailzai@gmail.com>
2015-07-19 23:06:20 -0400

    fixing sync issue

---
+++ src/view/gmapLabels.js
@@ -0,0 +1,60 @@
+/*jslint node: true */
+/*global socioscapes, module, google, require*/
+'use strict';
+/**
+ * This method creates a new google.maps.OverlayView and loads it on top of the other layers; all map elements except
+ * labels are hidden.
+ *
+ * @function viewGmap_Labels
+ * @memberof! socioscapes
+ * @param {Object} myMap - The map to append this OverlayView to.
+ * @param {Array} [styles] - An optional array of {"feature": "rule"} declarative styles for map labels.
+ * @return {Object} myMap - The rendered Google Maps object.
+ */
+module.exports = function viewGmap_Labels(myMap, styles) {
+    var callback = arguments[arguments.length - 1],
+        dom,
+        LayerHack;
+    callback = (typeof callback === 'function') ? callback:function(result) { return result; };
+    styles = styles || [
+        {
+            "elementType": "all",
+            "stylers": [
+                { "visibility": "off" }
+            ]
+        },  {
+            "featureType": "administrative",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                { "visibility": "on" },
+                { "color": "#ffffff" },
+                { "weight": 5 }
+            ]
+        },{
+            "featureType": "administrative",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                { "visibility": "on" },
+                { "color": "#000000" }
+            ]
+        }
+    ];
+
+    // Create a custom OverlayView class and declare rules that will ensure it appears above all other map content
+    LayerHack = new google.maps.OverlayView();
+    LayerHack.onAdd = function () {
+        dom = this.getPanes();
+        dom.mapPane.style.zIndex = 150;
+    };
+    LayerHack.onRemove = function () {
+        this.div_.parentNode.removeChild(this.div_);
+        this.div_ = null;
+    };
+    LayerHack.draw = function () { };
+    LayerHack.setMap(myMap);
+
+    // Create and set the label layer
+    myMap.labels = new google.maps.StyledMapType(styles);
+    myMap.overlayMapTypes.insertAt(0, myMap.labels);
+    callback(myMap);
+};
---
+++ src/view/gmapMap.js
@@ -0,0 +1,144 @@
+/*jslint node: true */
+/*global socioscapes, module, google, require*/
+'use strict';
+/**
+ * This method creates a new google.maps object and assigns it to the specified div.
+ *
+ * @function viewGmap_Map
+ * @memberof! socioscapes
+ * @param {Object} geocode - An object with latitude and longitude coordinates.
+ * @param {Object} geocode.lat - The latitude around which the map should be centered.
+ * @param {Object} geocode.long - The longitude around which the map should be centered.
+ * @param {Object} div - The html div element that will store the map ( document.getElementById('divId') ).
+ * @param {Array} styles - An optional array of {"feature": "rule"} declarative styles for map features.
+ * @param {Array} options - An array of valid Google Maps map option.
+ * @return {Object} myMap - The rendered Google Maps object.
+ */
+module.exports = function (geocode, div, styles, options) {
+    var callback = arguments[arguments.length - 1],
+        myMap;
+    callback = (typeof callback === 'function') ? callback:function(result) { return result; };
+    styles = styles || [
+        {
+            "featureType": "administrative",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {"color": "#444444"}
+            ]
+        },
+        {
+            "featureType": "administrative.locality",
+            "elementType": "labels.text",
+            "stylers":
+                [
+                    {"visibility": "on"}
+                ]
+        },
+        {
+            "featureType": "administrative.neighborhood",
+            "elementType": "labels.text",
+            "stylers":
+                [
+                    {"visibility": "off"},
+                    {"hue": "#ff0000"}
+                ]
+        },
+        {
+            "featureType": "landscape",
+            "elementType": "all",
+            "stylers":
+                [
+                    {"color": "#f2f2f2"}
+                ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "all",
+            "stylers":
+                [
+                    {"visibility": "off"}
+                ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "all",
+            "stylers":
+                [
+                    {"saturation": -100},
+                    {"lightness": 45}
+                ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "all",
+            "stylers":
+                [
+                    {"visibility": "simplified"}
+                ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.text",
+            "stylers":
+                [
+                    {"visibility": "on"}
+                ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.icon",
+            "stylers":
+                [
+                    {"visibility": "on"}
+                ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.icon",
+            "stylers":
+                [
+                    {"visibility": "off"}
+                ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "all",
+            "stylers":
+                [
+                    {"visibility": "off"}
+                ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers":
+                [
+                    {"color": "#46bcec"},
+                    {"visibility": "on"}
+                ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels",
+            "stylers":
+                [
+                    {"visibility": "off"}
+                ]
+        }
+    ];
+    options = options || {
+        zoom: 13,
+        center: new google.maps.LatLng(geocode.lat, geocode.long),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: true,
+        MapTypeControlOptions: {mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.SATELLITE], style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+        scaleControl: true,
+        disableDoubleClickZoom: true,
+        streetViewControl: true,
+        overviewMapControl: true,
+        styles: styles
+    };
+    myMap = new google.maps.Map(div, options);
+    myMap.setTilt(45);
+    callback(myMap);
+};
